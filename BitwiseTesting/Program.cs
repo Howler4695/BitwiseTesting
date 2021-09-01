f925855149797fc1502961c86472f3fe2c5c82ef
Hayden Howle <Howler4695@gmail.com>
2021-09-01 18:01:02 -0500

    Add project files.

---
+++ BitwiseTesting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BitwiseTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint int1 = 0b_0000_0000_0000_0000_0000_0000_0000_0001;
+            uint int2 = 0b_0000_0000_0000_0000_0000_0000_0000_0001;
+            Console.WriteLine(int1);
+            Console.WriteLine(Convert.ToString(int1, toBase: 2));
+            int1 ^= int1;
+            //intReverse = intReverse << 31;
+            //int2 = ~intReverse;
+            //intReverse = intReverse >> 32;
+            Console.WriteLine(int1);
+            Console.WriteLine(Convert.ToString(int1, toBase: 2));
+        }
+    }
+}
